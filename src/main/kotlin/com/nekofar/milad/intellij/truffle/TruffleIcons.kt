04fe5a1ce0394dee0fa41cd937f97215dd408a7f
Milad Nekofar <milad@nekofar.com>
2022-02-05 23:35:00 +0330

    feat: add project generation through `Truffle` cli

---
+++ src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleIcons.kt
@@ -0,0 +1,9 @@
+package com.nekofar.milad.intellij.truffle
+
+import com.intellij.openapi.util.IconLoader
+import javax.swing.Icon
+
+object TruffleIcons {
+    @JvmField
+    val ProjectGenerator: Icon = IconLoader.getIcon("/icons/truffle.png", javaClass)
+}
