d3f01914d55dc6cdd5d1a52a92146b689a4a547b
Milad Nekofar <milad@nekofar.com>
2022-02-08 22:22:28 +0330

    feat: register new file type for truffle configuration

---
+++ src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleFileType.kt
@@ -0,0 +1,13 @@
+package com.nekofar.milad.intellij.truffle
+
+import com.intellij.icons.AllIcons
+import com.intellij.lang.javascript.JavascriptLanguage
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+object TruffleFileType: LanguageFileType(JavascriptLanguage.INSTANCE) {
+    override fun getName(): String = TruffleBundle.message("truffle.file.type.name")
+    override fun getDescription(): String = TruffleBundle.message("truffle.file.type.description")
+    override fun getDefaultExtension(): String = "js"
+    override fun getIcon(): Icon = AllIcons.FileTypes.Config
+}
