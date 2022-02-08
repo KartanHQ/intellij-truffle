e2ce48d2a49e337acb86bea8d55165e08473ba5a
Milad Nekofar <milad@nekofar.com>
2022-02-08 22:00:02 +0330

    build: add intellij gradle qodana plugin for code quality checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.3.1"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "1.3.1"
+    // Gradle Qodana Plugin
+    id("org.jetbrains.qodana") version "0.1.13"
     // Kotlin support
     kotlin("jvm") version "1.6.10"
     java
@@ -38,6 +40,14 @@ changelog {
     version.set(properties("pluginVersion"))
     unreleasedTerm.set("[unreleased]")
     groups.set(emptyList())
+}
+
+// Configure Gradle Qodana Plugin - read more: https://github.com/JetBrains/gradle-qodana-plugin
+qodana {
+    cachePath.set(projectDir.resolve(".qodana").canonicalPath)
+    reportPath.set(projectDir.resolve("build/reports/inspections").canonicalPath)
+    saveReport.set(true)
+    showReport.set(System.getenv("QODANA_SHOW_REPORT")?.toBoolean() ?: false)
 }
 
 tasks {
