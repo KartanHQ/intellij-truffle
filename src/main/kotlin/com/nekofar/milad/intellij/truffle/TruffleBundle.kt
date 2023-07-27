258410c44b662516ba5fe5fddf3f3ba6eb29ba68
Milad Nekofar <milad@nekofar.com>
2023-07-27 21:16:14 +0330

    refactor: update codebase for improved readability and consistency.
    In this commit, we have refactored parts of the Truffle codebase for improved readability, clarity, and consistency. Specifically, we enhanced the code styling in TruffleBundle.kt and TruffleIcons.kt, converting the overridden methods from block to expression bodies for concise representation and removing unnecessary imports. Furthermore, we updated the icon naming from 'logo.svg' to 'truffle.svg' to better align with the current project's icon naming scheme.

--- src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleBundle.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleBundle.kt
@@ -1,13 +1,13 @@
 package com.nekofar.milad.intellij.truffle
 
 import com.intellij.DynamicBundle
-import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
+import org.jetbrains.annotations.NonNls
 
 @NonNls
 private const val BUNDLE = "messages.TruffleBundle"
 
-object TruffleBundle: DynamicBundle(BUNDLE) {
+object TruffleBundle : DynamicBundle(BUNDLE) {
     @JvmStatic
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
+    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) = getMessage(key, *params)
 }
--- src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleIcons.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/truffle/TruffleIcons.kt
@@ -1,12 +1,11 @@
 package com.nekofar.milad.intellij.truffle
 
 import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
 
 object TruffleIcons {
     @JvmField
     val FileType = IconLoader.getIcon("/icons/type.svg", javaClass)
 
     @JvmField
-    val ProjectGenerator: Icon = IconLoader.getIcon("/icons/logo.svg", javaClass)
+    val ProjectGenerator = IconLoader.getIcon("/icons/truffle.svg", javaClass)
 }
