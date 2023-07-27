8eb28aefab4085fe7b45c5b845303369ef0d95e8
Milad Nekofar <milad@nekofar.com>
2023-07-27 21:09:54 +0330

    refactor: simplify project generator overriding methods
    Simplified methods in TruffleCliProjectGenerator class by converting block bodies to expression bodies. This results in more concise and cleaner code, enhancing its readability and maintainability. The behavior of these methods remains unchanged with this refactoring.

--- src/main/kotlin/com/nekofar/milad/intellij/truffle/cli/TruffleCliProjectGenerator.kt
+++ src/main/kotlin/com/nekofar/milad/intellij/truffle/cli/TruffleCliProjectGenerator.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
 import com.nekofar.milad.intellij.truffle.TruffleBundle
+import com.nekofar.milad.intellij.truffle.TruffleBundle.message
 import com.nekofar.milad.intellij.truffle.TruffleIcons
 import javax.swing.Icon
 
@@ -15,37 +16,21 @@ class TruffleCliProjectGenerator: NpmPackageProjectGenerator() {
     private val executable = "truffle"
     private val initCommand = "init"
 
-    override fun getName(): String {
-        return TruffleBundle.message("truffle.project.generator.name")
-    }
+    override fun getName() = message("truffle.project.generator.name")
 
-    override fun getDescription(): String {
-        return TruffleBundle.message("truffle.project.generator.description")
-    }
+    override fun getDescription() = message("truffle.project.generator.description")
 
-    override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> {
-        return emptyArray()
-    }
+    override fun filters(project: Project, baseDir: VirtualFile) = emptyArray<Filter>()
 
     override fun customizeModule(p0: VirtualFile, p1: ContentEntry?) {}
 
-    override fun packageName(): String {
-        return packageName
-    }
+    override fun packageName() = packageName
 
-    override fun presentablePackageName(): String {
-        return TruffleBundle.message("truffle.project.generator.presentable.package.name")
-    }
+    override fun presentablePackageName() = message("truffle.project.generator.presentable.package.name")
 
-    override fun getNpxCommands(): List<NpxPackageDescriptor.NpxCommand> {
-        return listOf(NpxPackageDescriptor.NpxCommand(packageName, executable))
-    }
+    override fun getNpxCommands() = listOf(NpxPackageDescriptor.NpxCommand(packageName, executable))
 
-    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?): Array<String> {
-        return arrayOf(initCommand)
-    }
+    override fun generatorArgs(project: Project?, dir: VirtualFile?, settings: Settings?) = arrayOf(initCommand)
 
-    override fun getIcon(): Icon {
-        return TruffleIcons.ProjectGenerator
-    }
+    override fun getIcon() = TruffleIcons.ProjectGenerator
 }
